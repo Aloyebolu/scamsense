480704b44c4b90c86587dcf0a9a210c4f90a0aec
AloyeBolu <aloyebolu5@gmail.com>
2026-02-10 09:04:06 +0100

    Another commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-'use client';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -6,15 +5,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className="light">
       <body className={`${inter.className} bg-background text-foreground min-h-screen`}>
-        <main className="min-h-screen">
-          {children}
-        </main>
+        <main className="min-h-screen">{children}</main>
       </body>
     </html>
   );
